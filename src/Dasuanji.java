209ea133c3b751cd0be19c8d29c8cf45a571ddb8
Fetihonl <46765862+Fetihonl@users.noreply.github.com>
2019-01-20 20:04:01 -0500

    Add multiply method.

--- src/Dasuanji.java
+++ src/Dasuanji.java
@@ -8,4 +8,6 @@ public class Dasuanji {
         double result = num1 - num2;
         return num1-num2;
     }
+    public static double multiply(double num1, double num2){
+        return num1*num2;
 }
