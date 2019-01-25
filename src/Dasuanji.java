61b1515578c167c90550657eeb138f127875e18f
Fetihonl <myEmail@Gmail.com>
2019-01-25 16:04:55 -0500

    add comment to add method

--- src/Dasuanji.java
+++ src/Dasuanji.java
@@ -1,13 +1,17 @@
 public class Dasuanji {
+    //this is a very useful method
 
-    public static double add(double num1, double num2){
-        return num1+num2;
+    public static double add(double num1, double num2) {
+
+        return num1 + num2;
     }
 
-    public static double subtract(double num1, double num2){
+    public static double subtract(double num1, double num2) {
         double result = num1 - num2;
-        return num1-num2;
+        return num1 - num2;
     }
-    public static double multiply(double num1, double num2){
-        return num1*num2;
+
+    public static double multiply(double num1, double num2) {
+        return num1 * num2;
+    }
 }
